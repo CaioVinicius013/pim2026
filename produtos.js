cbf0a7ad0c8bca409ea9468217f3661a02b46d03
caio <caiobvini31@gmail.com>
2026-06-08 00:03:52 -0300

    tentei arrumar

--- produtos.js
+++ produtos.js
@@ -70,64 +70,59 @@ function renderizarProdutos(produtos) {
         // URL da imagem
         const imagemUrl =
 
-`https://sublime-abdominal-pureness.ngrok-free.dev/imagens/${produto.urlImagem}`;
+        `https://sublime-abdominal-pureness.ngrok-free.dev/imagens/${produto.urlImagem}`;
 
-        card.innerHTML = `
+            card.innerHTML = `
 
-            <a
-                href="
-infoprodutos.html?id=${produto.idProduto}
-                "
-                class="
-                produto-link
-                "
-            >
+        <div class="produto-link">
 
+            <a href="infoprodutos.html?id=${produto.idProduto}">
                 <img
                     src="${imagemUrl}"
-                    class="
-                    produto-img
-                    "
-                    alt="
-                    ${produto.nmProduto}
-                    "
-
-                    
+                    class="produto-img"
+                    alt="${produto.nmProduto}"
                 >
 
-                <div
-                    class="
-                    produto-info
-                    "
-                >
+                <div class="produto-info">
 
-                    <h3>
-                        ${produto.nmProduto}
-                    </h3>
+                    <h3>${produto.nmProduto}</h3>
 
-                    <p>
-                        ${produto.dsProduto}
-                    </p>
+                    <p>${produto.dsProduto}</p>
 
-                    <div
-                        class="
-                        preco
-                        "
-                    >
-                        R$
-                        ${produto.vlPreco}
+                    <div class="preco">
+                        R$ ${produto.vlPreco}
                     </div>
-
                 </div>
-
             </a>
-        `;
+
+          <button
+            class="btn-comprar"
+            data-nome="${produto.nmProduto}"
+            data-preco="${produto.vlPreco}"
+            data-img="${imagemUrl}"
+          >
+            Comprar
+         </button>
+
+        </div>
+    `;
 
         container.appendChild(
             card
         );
     });
 }
+document.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-comprar');
+    if (!btn) return;
 
+    const name = btn.dataset.nome;
+    const price = parseFloat(btn.dataset.preco);
+    const img = btn.dataset.img;
+
+    console.log('comprei:', name);
+
+    addToCart(name, price, img);
+});
 
 carregarProdutos();
